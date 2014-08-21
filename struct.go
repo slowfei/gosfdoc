ac743f38da2525b86bb9b2875ce56561eac8de4c
slowfei <slowfei@foxmail.com>
2014-08-22 03:13:36 +0800

    将一些信息存储的结构体放置一个文件中

---
+++ struct.go
@@ -0,0 +1,17 @@
+package gosfdoc
+
+/**
+ * package info
+ */
+type PackageInfo struct {
+	Name string // package name
+	Desc string // description
+}
+
+/**
+ *  document struct info
+ */
+type Document struct {
+	Title   string
+	Content string
+}
