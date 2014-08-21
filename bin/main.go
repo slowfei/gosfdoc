709c4742ae2d21cc50411d668bfe0939c50c062c
slowfei <slowfei@foxmail.com>
2014-08-21 18:25:53 +0800

    更改执行文件的实现

---
+++ bin/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/slowfei/gosfdoc"
+	"strings"
+)
+
+var (
+	_lang       *string = nil
+	_configFile         = flag.String("config", "gosfdoc.json", "custom config file path.")
+)
+
+func init() {
+	impls := make([]string, 0, 0)
+	for k, _ := range gosfdoc.MapParser() {
+		impls = append(impls, k)
+	}
+	implstr := strings.Join(impls, ",")
+	_lang = flag.String("lang", "", "[\""+implstr+"\"] specify code language type, default all language.")
+}
+
+/**
+ *  print usage help
+ */
+func usage() {
+	fmt.Println(gosfdoc.APPNAME, "v"+gosfdoc.VERSION)
+	fmt.Println("")
+	fmt.Println("usage help:")
+	fmt.Println("'" + gosfdoc.APPNAME + " create' command init create default gosfdoc.json file, can be custom to modify file content.")
+	fmt.Println("'" + gosfdoc.APPNAME + "' command by gosfdoc.json output document ")
+	fmt.Println("")
+	fmt.Println("other param:")
+	flag.PrintDefaults()
+	fmt.Println("")
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	arg := flag.Args()[0]
+
+	if 0 != len(arg) {
+		switch arg {
+		case "help":
+			flag.Usage()
+		case "version":
+			fmt.Println(gosfdoc.APPNAME, "v"+gosfdoc.VERSION)
+		case "create":
+
+		case "output":
+
+		default:
+			fmt.Println("invalid command parameter.")
+		}
+		return
+	}
+
+}
