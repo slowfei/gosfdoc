e4080690f7c1f264954b12954549f9ad863b0803
slowfei <slowfei@foxmail.com>
2014-09-08 04:22:15 +0800

    About、Intro解析操作
    已经经过测试

--- gosfdoc_test.go
+++ gosfdoc_test.go
@@ -1,1 +1,109 @@
 package gosfdoc
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestREXPrivateBlock(t *testing.T) {
+	str := `
+func test1(){
+}
+//#private
+func test2(){
+}
+//#private-end
+func test3(){
+}
+//#private
+func test4(){
+}
+//#private-end
+func test5(){
+}
+    `
+	strResult := `
+func test1(){
+}
+func test3(){
+}
+func test5(){
+}
+    `
+	bf := NewFileBuf([]byte(str), REXPrivateBlock)
+
+	if bf.String() != strResult {
+		t.Fatalf("private block replace failed.")
+	}
+}
+
+func TestREXParseAboutAndIntro(t *testing.T) {
+	strTest := `
+
+func func_name() {
+    
+}
+
+//About
+//  ## About
+//  ------
+//  
+//  gosfdoc document generator
+// 
+//  More references: [https://github.com/slowfei/gosfdoc][0]<br/>
+//  The MIT license (MIT) - [http://opensource.org/licenses/MIT][1]
+//
+//  Copyright (c) 2014 slowfei<br/>
+//  Email: slowfei#foxmail.com
+//
+//  [0]:https://github.com/slowfei/gosfdoc
+//  [1]:http://opensource.org/licenses/MIT
+//  [2]:http://opensource.org/licenses/MIT3
+//End
+
+/**About
+ *  ## About
+ *  ------
+ *  
+ *  gosfdoc document generator
+ * 
+ *  More references: [https://github.com/slowfei/gosfdoc][0]<br/>
+ *  The MIT license (MIT) - [http://opensource.org/licenses/MIT][1]
+ *
+ *  Copyright (c) 2014 slowfei<br/>
+ *  Email: slowfei#foxmail.com
+ *
+ *  [0]:https://github.com/slowfei/gosfdoc
+ *  [1]:http://opensource.org/licenses/MIT
+ *  [2]:http://opensource.org/licenses/MIT2
+ */
+
+
+/**Intro
+ *  ## Intro
+ *  ------
+ *  
+ *  Intro content
+ */
+
+ func func_name() {
+     
+ }
+`
+
+	newBuf := ParseAbout(NewFileBuf([]byte(strTest), nil))
+	lineNumber := len(bytes.Split(newBuf, []byte("\n")))
+
+	if 15 != lineNumber {
+		// t.Log(string(newBuf))
+		t.Fatal("About: Target Line 14, parse Line error:", lineNumber)
+	}
+
+	newBuf = ParseIntro(NewFileBuf([]byte(strTest), nil))
+	lineNumber = len(bytes.Split(newBuf, []byte("\n")))
+	if 5 != lineNumber {
+		t.Log(string(newBuf))
+		t.Fatal("Intro: Target Line 5, parse Line error:", lineNumber)
+	}
+
+}
---
+++ parse.go
@@ -0,0 +1,149 @@
+package gosfdoc
+
+import (
+	"bytes"
+	"regexp"
+	// "fmt"
+)
+
+var (
+	_defaultAbout = []byte(`
+## About
+------
+
+gosfdoc document generator
+
+More references: [https://github.com/slowfei/gosfdoc][0]<br/>
+The MIT license (MIT) - [http://opensource.org/licenses/MIT][1]
+
+Copyright (c) 2014 slowfei<br/>
+Email: slowfei#foxmail.com
+
+[0]:https://github.com/slowfei/gosfdoc
+[1]:http://opensource.org/licenses/MIT
+`)
+
+	_defaultIntro = []byte(`
+##Document Introduction
+
+Sorry! Document author did not write any information.
+
+----
+This is a good tool, Can help you make beautiful documents.
+
+More references: [https://github.com/slowfei/gosfdoc][0]<br/>
+
+[0]:https://github.com/slowfei/gosfdoc
+`)
+
+	//  主要用于去除注释的前缀
+	_prefixFilterTags = [][]byte{
+		[]byte(" *\t"),
+		[]byte(" *  "),
+		[]byte(" * "),
+		[]byte("//\t"),
+		[]byte("//  "),
+		[]byte("// "),
+		[]byte("//"),
+	}
+
+	_tagStar   = []byte("*")
+	_tagDSlash = []byte("//")
+)
+
+/**
+ *  commons parse file about content
+ *
+ *  @param `fileBuf`
+ *  @return about content
+ */
+func ParseAbout(fileBuf *FileBuf) []byte {
+	return parseAboutAndIntro(fileBuf, REXAbout)
+}
+
+/**
+ *  commons parse file introduction content
+ *
+ *  @param `fileBuf`
+ *  @return introduction content
+ */
+func ParseIntro(fileBuf *FileBuf) []byte {
+	return parseAboutAndIntro(fileBuf, REXIntro)
+}
+
+/**
+ *  commons about intro parse
+ *
+ *  @param fileBuf
+ *  @param rex
+ */
+func parseAboutAndIntro(fileBuf *FileBuf, rex *regexp.Regexp) []byte {
+	var result []byte = nil
+	var prefixTag []byte = nil
+	prefixLen := 0
+
+	aboutBuf := fileBuf.Find(rex)
+
+	if 0 < len(aboutBuf) {
+		appendLine := bytes.NewBuffer(nil)
+
+		lines := bytes.Split(aboutBuf, []byte("\n"))
+		linesCount := len(lines)
+
+		for i := 1; i < linesCount-1; i++ {
+			newLine := lines[i]
+
+			if i == 1 {
+				//  记录第一个前缀的标识，以第一个为准，后面的根据要求都要是相同的注释前缀。
+				/**
+				  (*)remove prefix tag
+				  (*)
+				  (*)
+				*/
+				prefixTag = findPrefixFilterTag(newLine)
+				prefixLen = len(prefixTag)
+			}
+
+			if 0 != len(prefixTag) {
+
+				if 0 == bytes.Index(newLine, prefixTag) {
+					appendLine.Write(newLine[prefixLen:])
+				} else {
+					trimed := bytes.TrimSpace(newLine)
+					//  有可能存在空行，所以需要过滤只存在 "*" || "//"
+					if !bytes.Equal(trimed, _tagStar) && !bytes.Equal(trimed, _tagDSlash) {
+						appendLine.Write(newLine)
+					}
+				}
+
+			} else {
+				appendLine.Write(newLine)
+			}
+
+			appendLine.WriteByte('\n')
+		}
+
+		if 0 < appendLine.Len() {
+			result = appendLine.Bytes()
+		}
+	}
+
+	return result
+}
+
+/**
+ *  find prefix filter tag index
+ *  see var _prefixFilterTags
+ */
+func findPrefixFilterTag(src []byte) []byte {
+	var pftCount = len(_prefixFilterTags)
+
+	for i := 0; i < pftCount; i++ {
+		prefix := _prefixFilterTags[i]
+		if 0 == bytes.Index(src, prefix) {
+			return prefix
+		}
+	}
+
+	return nil
+}
