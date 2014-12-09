5837c81a5c37ff46cb7782a3d6704855bbdc6662
slowfei <slowfei@foxmail.com>
2014-12-09 11:07:39 +0800

    完善：数据存储操作接口

--- index/index.go
+++ index/index.go
@@ -3,20 +3,89 @@
 //  Copyright (c) 2014 slowfei
 //
 //  Create on 2014-11-28
-//  Update on 2014-11-28
+//  Update on 2014-12-03
 //  Email  slowfei(#)foxmail.com
 //  Home   http://www.slowfei.com
 
 //
-//  golang type index data storage systems
+//  type index data storage systems
 //
 package index
 
+import (
+	"errors"
+)
+
+// database type
+type DBType int
+
+// database type definit
+const (
+	DBTypeMemory DBType = iota // disposable memory cache
+	DBTypeFile                 // file type storage
+)
+
+// error definit
+var (
+	ErrInvalidIndex = errors.New("gosfdoc/index: Invalid unique index, package or type name nil.")
+)
+
 /**
- *
+ *  data storage interface
  */
 type IndexDB interface {
+
+	/**
+	 *  Open (operating data)-> Close -> Open (operating data)-> Close...
+	 *
+	 *  @return `error`
+	 */
+	Open() error
+
+	/**
+	 *  all finished operating data can close
+	 */
+	Close()
+
+	/**
+	 *  save as type info, the same data is overwritten
+	 *  package and name identifies a unique index
+	 *
+	 *  @param `t`
+	 *  @return `error`
+	 */
+	SetType(t TypeInfo) error
+
+	/**
+	 *  by package and type name get type info
+	 *
+	 *  @param `packageName` pacaage
+	 *  @param `typeName` type name
+	 *  @return `TypeInfo`
+	 *  @return `bool`
+	 */
+	Type(packageName, typeName string) (TypeInfo, bool)
 }
 
-type Types struct {
+/**
+ *  open or create IndexDB
+ *
+ *  @param `langName` language name string
+ *  @param `dbT`      storage type, default DBTypeMemory
+ */
+func CreateIndexDB(langName string, dbT DBType) IndexDB {
+	//  TODO 暂时只实现了内存存储，一次性的。
+	return initMenDB(langName)
+}
+
+/**
+ *  index type struct
+ *  type info in various languages
+ */
+type TypeInfo struct {
+	DocHttpUrl string // document http url e.g.: http://slowfei.github.io/gosfdoc
+	Package    string // package and name identifies a unique index
+	Name       string // package and name identifies a unique index
+	LineStart  int    // line number start
+	LineEnd    int    // line number end
 }
